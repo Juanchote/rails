11ef749c996323a93f46277d6bf331a9df872971
Juan Manuel Rodulfo <jmrodulfo@outlook.com>
2014-05-06 11:41:58 +0200

    Added SSL

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,12 +16,14 @@ class UsersController < ApplicationController
 	  end
   end
 
+  def show
+	  @user = User.find(params[:id])
+  end
+
+  private
+
   def user_params
 	  params.require(:user).permit(:name, :email, :password,
 	                               :password_confirmation)
   end
-
-	def show
-		@user = User.find(params[:id])
-	end
 end
